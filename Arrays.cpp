1c859bbf5d12d8c9d6888f202066fdccf2ad9b57
Yogesh Suresh <yogeshsofficial@gmail.com>
2025-06-09 23:01:48 +0800

    Initial Commit

---
+++ Arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void main()
+{
+	cout << "Hello world! inside Arrays.cpp \n";
+	int array[10];
+	char chArray[10];
+	if (sizeof(array[0]))
+	{
+		for (int i = 0; i < sizeof(chArray) / sizeof(chArray[0]); i++)
+		{
+			chArray[i] = 'A';
+			cout << "array[" << i << "] is: " << chArray[i] << "\n";
+		}//End of for loop
+
+	}
+}
